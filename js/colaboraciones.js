26a308527a7405be88de008386d2e5e31767750d
Francisco Vergara <fvergara@drivetech.pro>
2025-10-19 23:29:27 -0300

    fix(apoyo): Se corrigen metodologías

--- js/colaboraciones.js
+++ js/colaboraciones.js
@@ -34,43 +34,43 @@ export const donations = [
 export const empresas = [
     {
         name: "Revista Pedalea",
-        method: "Partner Comunicacional",
+        method: "Comunicaciones",
         logo: "imgs/emp/Logotipo.pedalea.fondo_.blanco.png",
         url: "https://revistapedalea.com/"
     },
     {
         name: "Arlegui TV",
-        method: "Partner Comunicacional",
+        method: "Comunicaciones",
         logo: "imgs/emp/arleguitv.jpeg",
         url: "https://www.arleguitv.com/"
     },
     {
         name: "El Plan TV",
-        method: "Partner Comunicacional",
+        method: "Comunicaciones",
         logo: "imgs/emp/elplan.png",
         url: "https://elplan.cl/"
     },
     {
         name: "Quiltro",
-        method: "Partner Ecológico",
+        method: "Reciclaje y reutilización",
         logo: "imgs/emp/quiltro.jpg",
         url: "https://www.instagram.com/quiltro.costura/"
     },
     {
         name: "Concejalía Lukas Cáceres",
-        method: "Partner de Gestiones",
+        method: "Gestiones",
         logo: "imgs/emp/lukas_caceres.png",
         url: "https://linktr.ee/lukas.concejalvalpo2024"
     },
     {
         name: "Casa Volante Hostal",
-        method: "Partner de Estadía",
+        method: "Estadía y turismo",
         logo: "imgs/emp/casa_volante_hostal.png",
         url: "http://www.casavolantehostal.com/"
     },
     {
         name: "Turismo y Pedal",
-        method: "Partner de bicicletas",
+        method: "Bicicletas",
         logo: "imgs/emp/turismo_y_pedal.png",
         url: "https://www.instagram.com/bicidelmar/"
     },

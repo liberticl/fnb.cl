9617ac718738cb17a612d2ba4163766f9c0dbfd2
Francisco Vergara <fvergara@drivetech.pro>
2025-10-23 00:16:32 -0300

    feat(programa): se agrega parte de los logos faltantes y mejora estética

--- js/programa.js
+++ js/programa.js
@@ -9,11 +9,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const createEventCard = (event, index) => {
         if (!event.title) return '';
-    
+
         const cardId = `event-card-${index}`;
         const descriptionId = `description-${index}`;
         const logoId0 = `logo-0-${index}`
         const logoId1 = `logo-1-${index}`
+        const logoId2 = `logo-2-${index}`
+        const logoId3 = `logo-3-${index}`
+        const logoId4 = `logo-4-${index}`
 
         return `
             <div id="${cardId}" class="event-card bg-${event.type === 'cicletada' ? '[#4CAF5022]' : event.type === 'foro' ? '[#E567C722]' : event.type === 'noche' ? '[#FFEA8022]' : event.type === 'master' ? '[#6CB6FF22]' : 'white'} p-6 md:p-8 rounded-xl shadow-lg hover:shadow-xl transition-shadow duration-300 border-l-4 border-[#4A45B0] mb-6 ${event.ondate ? '' : 'hidden'}">
@@ -21,16 +24,33 @@ document.addEventListener('DOMContentLoaded', () => {
                     <div class="md:w-1/4 mb-4 md:mb-0 cursor-pointer">
                         <p class="text-sm font-bold text-[#E567C7] uppercase">${event.day}</p>
                         <p class="text-3xl font-extrabold text-[#212121]">${event.time}</p>
-                        <div class="w-full flex justify-left items-left space-x-2">
-                            <img id="${logoId0}" src="../imgs/programa/${event.logo[0]}.png" alt="Logo ${event.org}" class="w-40 h-24 object-contain logo-0-content collapsed ${event.logo[0] ? '' : 'hidden'}">
-                            <img id="${logoId1}" src="../imgs/programa/${event.logo[1]}.png" alt="Logo ${event.org}" class="w-40 h-24 object-contain logo-1-content collapsed ${event.logo[1] ? '' : 'hidden'}">
+                        <div class="flex flex-wrap justify-left items-center w-full">
+                            <div class="p-2 w-1/2 md:w-1/4 lg:w-1/5 min-w-[120px] ${event.logo[0] ? '' : 'hidden'}">
+                                <img id="${logoId0}" src="../imgs/programa/${event.logo[0]}.png" alt="Logo ${event.org}" class="h-32 object-contain mx-auto logo-0-content collapsed">
+                            </div>
+
+                            <div class="p-2 w-1/2 md:w-1/4 lg:w-1/5 min-w-[120px] ${event.logo[1] ? '' : 'hidden'}">
+                                <img id="${logoId1}" src="../imgs/programa/${event.logo[1]}.png" alt="Logo ${event.org}" class="h-32 object-contain mx-auto logo-1-content collapsed">
+                            </div>
+
+                            <div class="p-2 w-1/2 md:w-1/4 lg:w-1/5 min-w-[120px] ${event.logo[2] ? '' : 'hidden'}">
+                                <img id="${logoId2}" src="../imgs/programa/${event.logo[2]}.png" alt="Logo ${event.org}" class="h-32 object-contain mx-auto logo-2-content collapsed">
+                            </div>
+
+                            <div class="p-2 w-1/2 md:w-1/4 lg:w-1/5 min-w-[120px] ${event.logo[3] ? '' : 'hidden'}">
+                                <img id="${logoId3}" src="../imgs/programa/${event.logo[3]}.png" alt="Logo ${event.org}" class="h-32 object-contain mx-auto logo-3-content collapsed">
+                            </div>
+
+                            <div class="p-2 w-full flex justify-center ${event.logo[4] ? '' : 'hidden'}">
+                                <img id="${logoId4}" src="../imgs/programa/${event.logo[4]}.png" alt="Logo ${event.org}" class="h-32 object-contain mx-auto logo-4-content collapsed">
+                            </div>
                         </div>
                     </div>
                     <div class="md:w-3/4">
                         <h3 class="text-2xl font-bold text-[#4A45B0] mb-0 cursor-pointer">${event.title}</h3>
                         <p class="text-xl font-semibold text-[#4CAF50] mb-0 cursor-pointer">${event.speaker}</p>
                         <p class="text-xl font-semibold text-[#4CAF50AA] mb-0 cursor-pointer">${event.org}</p>
-                        <p id="${descriptionId}" class="description-content collapsed text-lg whitespace-pre-wrap">${event.description}</p>
+                        <div id="${descriptionId}" class="description-content collapsed text-lg whitespace-pre-wrap">${event.description}</div>
                     </div>
                 </div>
             </div>
@@ -44,11 +64,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 const logo0 = card.querySelector('.logo-0-content');
                 const logo1 = card.querySelector('.logo-1-content');
+                const logo2 = card.querySelector('.logo-2-content');
+                const logo3 = card.querySelector('.logo-3-content');
+                const logo4 = card.querySelector('.logo-4-content');
                 const description = card.querySelector('.description-content');
 
                 if (description) {
                     logo0.classList.toggle('collapsed');
                     logo1.classList.toggle('collapsed');
+                    logo2.classList.toggle('collapsed');
+                    logo3.classList.toggle('collapsed');
+                    logo4.classList.toggle('collapsed');
                     description.classList.toggle('collapsed');
                 }
             });
